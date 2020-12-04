79e14b42198c553f0a3935fbecc503efffef3024
welington <welington_jonathan@hotmail.com>
2020-12-04 19:21:29 -0300

    implementação de Cards

--- projetoIndividual_REACT/src/views/produtos.js
+++ projetoIndividual_REACT/src/views/produtos.js
@@ -2,6 +2,7 @@ import { useState, useEffect} from 'react';
 import Categoria from '../componente/Produto/categoria.js';
 import 'bootstrap/dist/css/bootstrap.css'
 import '../componente/Produto/produto.css'
+import {Card, CardDeck, Button} from 'react-bootstrap';
 
 export default function Produtos(){
     const [ produtos, setProdutos] = useState([]);
@@ -17,19 +18,28 @@ export default function Produtos(){
 
     return(
                 <div className="d-flex">
-                <Categoria />
-                        <section className="produtos">
-                            {produtos && produtos.map( ({ id_produto, categoria, descricao, preco, preco_venda, imagem_produto }) => (
-                            <div key={id_produto} >
-                                <img id="imagem_produto" src={require(`../componente/Produto/img/foto_nao_disponivel.gif`).default} alt={categoria}/>
-                                <h3>{descricao}</h3>
+                    <Categoria />
+                    
+                    <section className="produtos">
+                    {produtos && produtos.map( ({ id_produto, categoria, descricao, preco, preco_venda,}) => (<Card className="text-center" style={{width: '11rem'}}>
+                        <Card.Img variant="top" src={require(`../componente/Produto/img/foto_nao_disponivel.gif`).default} alt={categoria} />
+                        <Card.Body>
+                            <Card.Title>{categoria}</Card.Title>
+                            <Card.Text>
+                                <p>{descricao}</p>
                                 <p className="precoOriginal">R$ {preco}</p>
                                 <p className="newPrice">R$ {preco_venda}</p>
-                            </div>
-                                )
-                            )}
-                        </section>
-                        <hr/>
+                            </Card.Text>
+                            <Button variant="success">Comprar</Button>
+                        </Card.Body>
+                    </Card>
+                    )
+                    )}
+                    </section>
+                    
+                    
+                    
+                    
                 </div>
                  
     )
